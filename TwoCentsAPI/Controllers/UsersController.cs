822aa46aed65498b4392873c58c58c8ef4d97e96
Jacqueline Remmel <jacqueline@>
2019-12-17 10:43:58 -0800

    Add backend users controller

--- TwoCentsAPI/Controllers/UsersController.cs
+++ TwoCentsAPI/Controllers/UsersController.cs
@@ -3,43 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using TwoCentsAPI.Models;
+using TwoCentsAPI.Services;
 
 namespace TwoCentsAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class UsersController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private IUserService _userService;
+        private readonly TwoCentsAPIContext _db;
+
+        public UsersController(IUserService userService, TwoCentsAPIContext db)
         {
-            return new string[] { "value1", "value2" };
+            _userService = userService;
+            _db = db;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User userLoggingIn)
         {
-            return "value";
+            var user = _userService.Authenticate(userLoggingIn.Email, userLoggingIn.Password);
+            if (user == null)
+                return BadRequest(new { message = "Email or password is incorrect." });
+            return Ok(user);
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [AllowAnonymous]
+        [HttpPost("create")]
+        public void Create([FromBody] User newUser)
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            //only save hashed password in database
+            var passwordHasher = new PasswordHasher<TwoCentsAPI.Models.User>();
+            newUser.PasswordHash = passwordHasher.HashPassword(newUser, newUser.Password);
+            newUser.Password = null;
+            _db.Users.Add(newUser);
+            _db.SaveChanges();
         }
     }
 }
