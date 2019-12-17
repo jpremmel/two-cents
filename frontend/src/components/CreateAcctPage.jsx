b5586c67819b9e6e9a5179d4f16eab40b3568e44
Jacqueline Remmel <jacqueline@>
2019-12-17 14:06:47 -0800

    Update login and create acct forms

--- frontend/src/components/CreateAcctPage.jsx
+++ frontend/src/components/CreateAcctPage.jsx
@@ -1,28 +1,83 @@
 import React from 'react';
 
-const CreateAcctPage = () => {
-  return (
-    <div>
-      <h3>Create An Account</h3>
-      <form>
-        <input type='text' placeholder='First name' className='form-control' required='required' />
-        <br />
-        <input type='text' placeholder='Last name' className='form-control' required='required' />
-        <br />
-        <input type='text' placeholder='Email address' className='form-control' required='required' />
-        <br />
-        <input type='password' placeholder='Password' className='form-control' required='required' />
-        <br />
-        <input type='text' placeholder='LinkedIn URL' className='form-control' />
-        <br />
-        <input type='text' placeholder='Other URL (ie. personal website, portfolio)' className='form-control' />
-        <br />
-        <textarea type='text' placeholder='Say a bit about yourself' className='form-control' />
-        <br />
-        <button type='submit' className='btn btn-light'>Create Account</button>
-      </form>
-    </div>
-  );
+class CreateAcctPage extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    linkedin: '',
+    otherUrl: '',
+    bio: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create An Account</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            id='firstName'
+            type='text' 
+            placeholder='First name' 
+            className='form-control' 
+            required='required'
+            onChange={this.handleChange} />
+          <input 
+            id='lastName'
+            type='text' 
+            placeholder='Last name' 
+            className='form-control' 
+            required='required'
+            onChange={this.handleChange} />
+          <input 
+            id='email'
+            type='text' 
+            placeholder='Email address' 
+            className='form-control' 
+            required='required'
+            onChange={this.handleChange} />
+          <input 
+            id='password'
+            type='password' 
+            placeholder='Password' 
+            className='form-control' 
+            required='required'
+            onChange={this.handleChange} />
+          <input 
+            id='linkedin'
+            type='text' 
+            placeholder='LinkedIn URL' 
+            className='form-control'
+            onChange={this.handleChange} />
+          <input 
+            id='otherUrl'
+            type='text' 
+            placeholder='Other URL (ie. personal website, portfolio)' 
+            className='form-control'
+            onChange={this.handleChange} />
+          <textarea 
+            id='bio'
+            type='text' 
+            placeholder='Say a bit about yourself' 
+            className='form-control'
+            onChange={this.handleChange} />
+          <button type='submit' className='btn btn-light'>Create Account</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default CreateAcctPage;
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { login } from '../actions';
 
-const LoginPage = () => {
-  return (
-    <div>
-      <h3>Log in</h3>
-      <form>
-        <input type='text' placeholder='Email address' className='form-control' required='required' />
-        <br />
-        <input type='password' placeholder='Password' className='form-control' required='required' />
-        <br />
-        <button type='submit' className='btn btn-light'>Log in</button>
-      </form>
-    </div>
-  );
+class LoginPage extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Log in</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            id='email'
+            type='email' 
+            placeholder='Email address' 
+            className='form-control' 
+            required='required'
+            onChange={this.handleChange} />
+          <input 
+            id='password' 
+            type='password' 
+            placeholder='Password' 
+            className='form-control' 
+            required='required'
+            onChange={this.handleChange} />
+          <button /*type='submit'*/ className='btn btn-light'>Log in</button>
+        </form>
+      </div>
+    );
+  }
 };
 
-LoginPage.propTypes = {
-  dispatch: PropTypes.func
-};
+// LoginPage.propTypes = {
+//   dispatch: PropTypes.func
+// };
 
-//mapDispatchToProps?
-
-export default connect()(LoginPage);
+// export default connect()(LoginPage);
+export default LoginPage;
