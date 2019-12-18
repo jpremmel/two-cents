9ec2cb71139c770ade19b097b8822db0ab943066
Jacqueline Remmel <jacqueline@>
2019-12-17 16:46:53 -0800

    Add methods for creating new user, still need to make sure they work

--- frontend/src/actions/ApiHelper.js
+++ frontend/src/actions/ApiHelper.js
@@ -1,13 +1,27 @@
 export default class ApiHelper {
 
-    apiAttemptLogin(credentials) {
-        return new Promise(function(resolve, reject) {
+    apiCreateNewUser = (newUser) => {
+        let url = 'http://localhost:5000/api/Users/create';
+        let request = new XMLHttpRequest();
+        let body = JSON.stringify(newUser);
+        request.open('POST', url, true);
+        request.setRequestHeader('Content-Type', 'application/JSON');
+        request.onreadystatechange = () => {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log('Successfully created new user.');
+            }
+        }
+        request.send(body);
+    }
+
+    apiAttemptLogin = (credentials) => {
+        return new Promise((resolve, reject) => {
             let url = 'http://localhost:5000/api/Users/authenticate';
             let request = new XMLHttpRequest();
             let body = JSON.stringify(credentials);
             request.open('POST', url, true);
             request.setRequestHeader('Content-Type', 'application/JSON');
-            request.onload = function() {
+            request.onload = () => {
                 if (this.status === 200) {
                     resolve(request.response);
                 } else {
@@ -16,7 +30,6 @@ export default class ApiHelper {
             };
             request.send(body);
         });
-
     }
 
 }
--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,9 +1,8 @@
 import ApiHelper from './ApiHelper';
-
 const api = new ApiHelper();
 
 export const attemptLogin = (credentials) => {
-  return (dispatch) => {
+  return dispatch => {
     console.log(credentials);
     return api.apiAttemptLogin(credentials).then(response => {
         let parsedResponse = JSON.parse(response);
@@ -13,9 +12,9 @@ export const attemptLogin = (credentials) => {
       .then(parsedResponse => {
         console.log(parsedResponse);
         dispatch(login(parsedResponse));
-    }, error => {
-      console.log('Error occurred', error);
-    });
+      }, error => {
+        console.log('Error occurred', error);
+      });
   };
 }
 
@@ -27,3 +26,20 @@ export const login = user => ({
 export const logout = () => ({
   type: 'LOG_OUT'
 });
+
+export const createUser = (newUser) => {
+  return dispatch => {
+    console.log(newUser);
+    return api.apiCreateNewUser(newUser).then(response => {
+      let parsedResponse = JSON.parse(response);
+      console.log('JSON RESPONSE: ', parsedResponse);
+      return parsedResponse;
+    }, error => console.log('Error occurred', error))
+    .then(parsedResponse => {
+      console.log(parsedResponse);
+
+    }, error => {
+      console.log('Error occurred', error);
+    });
+  };
+}
